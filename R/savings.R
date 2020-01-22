b84961ec5eb0449df566ba7273d47fd9146c9f56
Andrew_AWS <andrew.royal@res-intel.com>
2020-01-22 11:33:44 -0500

    fix savings

--- R/savings.R
+++ R/savings.R
@@ -2,24 +2,33 @@
 #' @import data.table
 #' @export
 ebSavings = function(x, deemed, groups = NULL, sites_subset = NULL) {
+  x = copy(elct_sum)
   dat = merge(
     x$sites$savings[, .(site, Gross = gross, var_gross)],
     x$sites$metrics[, .(site, Baseline = baseline)])
   dat[, Deemed:= deemed[as.character(site)]]
   dat[, norm:= FALSE]
   if(!is.null(x$sites$norm)) {
-    dat = rbind(dat, x$sites$norms[, .(site, Gross = gross, var_gross, norm = TRUE)], fill = TRUE)
+    norm_dat = merge(
+      x$sites$norms[, .(site, Gross = gross, var_gross)],
+      x$sites$metrics[, .(site, Baseline = baseline)])
+    norm_dat[, Deemed:= deemed[as.character(site)]]
+    norm_dat[, norm:= TRUE]
+    dat = rbind(dat, norm_dat)
   }
   if(!is.null(sites_subset)) dat = dat[site %in% sites_subset]
   cols = c('Gross', 'Baseline', 'Deemed', 'var_gross')
   dat = dat[, lapply(.SD, as.numeric), .SDcols = cols, by = .(id = site, norm)]
-  if(!is.null(groups)) dat = dat[, lapply(.SD, sum), .SDcols = cols, by = .(id = groups[id])]
-  formatted = dat[, .(id = id,
-                      Baseline = format(round(Baseline), big.mark = ','),
-                      Deemed = format(round(Deemed), big.mark = ','),
-                      `Deemed/Baseline` = round(Deemed / Baseline, 2),
-                      Gross = format(round(Gross), big.mark = ','),
-                      RRate = round(Gross / Deemed, 2))]
+  if(!is.null(groups)) dat = dat[, lapply(.SD, sum, na.rm = TRUE), .SDcols = cols,
+                                 by = .(id = groups[as.character(id)], norm)]
+
+  formatted = dat[!(norm),
+                  .(id = id,
+                    Baseline = format(round(Baseline), big.mark = ','),
+                    Deemed = format(round(Deemed), big.mark = ','),
+                    `Deemed/Baseline` = round(Deemed / Baseline, 2),
+                    Gross = format(round(Gross), big.mark = ','),
+                    RRate = round(Gross / Deemed, 2))]
   out = list(formatted = formatted,
              raw = dat)
   setattr(out, 'class', c('savings', 'list'))
@@ -58,27 +67,31 @@ ebSavings_ECM = function(x, site_dat, measures, reporting_subset = NULL) {
 #' Plot Savings
 #' @import data.table
 #' @export
-ebPlot.savings = function(x, units = 'kWh') {
+ebPlot.savings = function(x, units = 'kWh', label = FALSE) {
   dat = melt(x$raw[, -c('var_gross')], id.vars = c('id', 'norm'), value.name = 'use')
-  dat = na.omit(dat)
+  dat[variable %in% c('Baseline', 'Deemed'), norm:= FALSE]
+  dat = unique(na.omit(dat))
   dat = merge(dat,
-              x$raw[, .(variable = 'Gross', id, var_gross, norm)],
+              x$raw[, .(variable = 'Gross', id, var_gross, norm, Deemed)],
               by = c('id', 'variable', 'norm'),
               all.x = TRUE)
   dat[(norm), variable:= 'Norm']
+  dat[, rr:= round(use / Deemed, 2)]
   dat[, use:= use / 1e3]
-  dat[, var_gross:= var_gross / 1e6]; dat[is.na(var_gross), var_gross:= 0]
+  dat[, var_gross:= var_gross / 1e6]
+  dat[is.na(var_gross), var_gross:= 0]
   dat[, variable:= factor(variable, levels = c('Baseline', 'Deemed', 'Gross', 'Norm'))]
 
   colors_v = c('Baseline' = 'black', 'Deemed' = 'gray', 'Gross' = 'lightgreen', 'Norm' = 'royalblue')
   labels_v = c('Baseline' = 'Baseline', 'Deemed Savings' = 'gray', 'Gross' = 'Gross Savings',
                'Norm' = 'WN Savings')
-  ggplot(data = dat, aes(x = variable,
-                         y = use,
-                         color = variable,
-                         fill = variable,
-                         ymin = use - 1.96 * sqrt(var_gross),
-                         ymax = use + 1.96 * sqrt(var_gross))) +
+  GP = ggplot(data = dat, aes(x = variable,
+                              y = use,
+                              color = variable,
+                              fill = variable,
+                              label = rr,
+                              ymin = use - 1.96 * sqrt(var_gross),
+                              ymax = use + 1.96 * sqrt(var_gross))) +
     theme_minimal() +
     theme(legend.position = 'top') +
     theme(panel.grid = element_blank()) +
@@ -93,4 +106,7 @@ ebPlot.savings = function(x, units = 'kWh') {
     coord_flip() +
     geom_hline(yintercept = 0) +
     facet_wrap(~id, scale = 'free_x', ncol = 3)
+
+  if(label) return(GP + geom_label(fill = 'white', color = 'black', nudge_y = 100))
+  GP
 }
